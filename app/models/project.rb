1ab43f3f84e3d6552f787aeeab873a41086df423
codystringham <cs.negativ@gmail.com>
2016-03-16 21:17:58 -0600

    validates

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,4 +6,6 @@ class Project < ActiveRecord::Base
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, reject_if: :all_blank, allow_destroy: true
 
+  validates_presence_of :name, :date, :details
+
 end
